d7058d0693343b85725c069dc09b84d40723503c
Andy Dobbs <andrew.dobbs@gmail.com>
2023-11-07 00:14:59 +0000

    Add CommonJS import examples

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
     res.end('Hello World!\n');
@@ -6,4 +7,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
     console.log('Server is running...');
+    console.log(_.random(1, 99));
 });
